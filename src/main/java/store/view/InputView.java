77bc62c3a01d6c22f3be6250b1470cd063f8bf5e
Goohong Chung <9rrong@uoslife.team>
2024-11-10 21:46:14 +0900

    feat(InputView): 프로모션 분기 처리 입력 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,8 +3,26 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
+    private static final String ORDER_INPUT_PROMPT =
+            System.lineSeparator() + "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String ADD_PROMOTION_PRODUCT_PROMPT = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
+
+    private static final String BUY_ONLY_PROMOTION_PRODUCTS_PROMPT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+
     public String readItem() {
-        System.out.println(System.lineSeparator() + "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println(ORDER_INPUT_PROMPT);
         return Console.readLine();
     }
+
+    public String promptAddPromotionProduct(String productName) {
+        System.out.printf(ADD_PROMOTION_PRODUCT_PROMPT, productName);
+        return Console.readLine();
+    }
+
+    public String promptBuyOnlyPromotionProduct(String productName, int nonPromotionQuantity) {
+        System.out.printf(BUY_ONLY_PROMOTION_PRODUCTS_PROMPT, productName, nonPromotionQuantity);
+        return Console.readLine();
+    }
+
 }
